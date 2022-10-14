f9d3d9eb37f54ac3c8a13de95460f1cb0dbfc0c5
git-bia19 <56919489+git-bia19@users.noreply.github.com>
2022-10-14 21:59:07 +0500

    final

---
+++ js/data/songs.js
@@ -0,0 +1,50 @@
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/Soldiers Of Allah.mp3",
+    "time": "03:22",
+    "title": "Soldiers of Allah",
+    "artist": "Unknown",
+    "image":"./images/soldiers-of-Allah.jpg"
+  },
+  {
+    "id": "2",
+    "url": "../../music/Ya Ilahi.mp3",
+    "time": "05:11",
+    "title": "Ya Ilahi",
+    "artist": "Ishaq Ayubi",
+    "image":"./images/Ya-ilahi.jpg"
+  },
+  {
+    "id": "3",
+    "url": "../../music/Ayesha Istiri.mp3",
+    "time": "03:07",
+    "title": "Ayesha Istiri",
+    "artist": "Muhammad Tarek",
+    "image":"./images/ayesha.jpg"
+  },
+  {
+    "id": "4",
+    "url": "../../music/aye-khuda.mp3",
+    "time": "06:18",
+    "title": "Aye Khuda",
+    "artist": "Omar Esa",
+    "image":"./images/aye-khuda.jpg"
+  },
+  {
+    "id": "5",
+    "url": "../../music/muhammad-nabina.mp3",
+    "time": "04:11",
+    "title": "Muhammad Nabina",
+    "artist": "Muhammad Tarek & Muhammad Yusuf",
+    "image":"./images/nabina.jpg"
+  },
+  {
+    "id": "6",
+    "url": "../../music/Qamarun.mp3",
+    "time": "03:51",
+    "title": "Qamarun",
+    "artist": "Faisal Latif",
+    "image":"./images/qamarun.jpg"
+  },
+ 
+];
